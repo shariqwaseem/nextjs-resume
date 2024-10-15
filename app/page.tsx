6b420811507e8e0a1ab530ce3e7c8b2094dacfc8
swaseem@creativechaos.co <mz7ncgZDJqPzBLNiRnxj>
2024-10-15 14:18:51 +0500

    Calendly replaced with Cal.com

--- app/page.tsx
+++ app/page.tsx
@@ -1,149 +1,146 @@
 import Image from "next/image";
 import ThemeSwitch from "@/components/theme-switch";
-import {generalData} from "@/data/general";
-import {contentData} from "@/data/content";
-import type {Content} from "@/data/content";
+import { generalData } from "@/data/general";
+import { contentData } from "@/data/content";
+import type { Content } from "@/data/content";
 import LinkifyText from "@/components/LinkifyText";
 
 type ContentProps = Content;
 
-const Content: React.FC<ContentProps> = ({title, items}) => {
-	return (
-		<section className="my-14 text-sm">
-			<h3 className="mb-6 font-semibold">{title}</h3>
-			<div className="flex flex-col gap-6">
-				{items.map((item, index) => {
-					return (
-						<div className="flex" key={index}>
-							{item.date && (
-								<div className="mr-8 max-w-[100px] w-full text-slate-400 dark:text-slate-400">
-									<LinkifyText textWithLink={item.date} />
-								</div>
-							)}
-							<div className="flex flex-col flex-1">
-								<h4>
-									<LinkifyText textWithLink={item.title} />
-								</h4>
-								<p className="text-slate-600 dark:text-gray-400">
-									{item.subTitle}
-								</p>
-								{item.description ? (
-									<LinkifyText
-										textWithLink={item.description}
-									/>
-								) : null}
-							</div>
-						</div>
-					);
-				})}
-			</div>
-		</section>
-	);
+const Content: React.FC<ContentProps> = ({ title, items }) => {
+  return (
+    <section className="my-14 text-sm">
+      <h3 className="mb-6 font-semibold">{title}</h3>
+      <div className="flex flex-col gap-6">
+        {items.map((item, index) => {
+          return (
+            <div className="flex" key={index}>
+              {item.date && (
+                <div className="mr-8 max-w-[100px] w-full text-slate-400 dark:text-slate-400">
+                  <LinkifyText textWithLink={item.date} />
+                </div>
+              )}
+              <div className="flex flex-col flex-1">
+                <h4>
+                  <LinkifyText textWithLink={item.title} />
+                </h4>
+                <p className="text-slate-600 dark:text-gray-400">
+                  {item.subTitle}
+                </p>
+                {item.description ? (
+                  <LinkifyText textWithLink={item.description} />
+                ) : null}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </section>
+  );
 };
 
 export default function Home() {
-	return (
-		<>
-			<main className="max-w-3xl mx-auto px-6 py-20 relative min-h-screen font-light">
-				<section className="flex items-center">
-					<Image
-						alt="Author"
-						src="/me.webp"
-						width={110}
-						height={110}
-						className="rounded-full object-cover"
-					/>
-					<div className="ml-4">
-						<h1 className="mb-0.5 text-xl text-slate-900 dark:text-slate-100">
-							{generalData.name}
-						</h1>
-						<h2 className="text-slate-600 dark:text-slate-300 text-sm">
-							{generalData.jobTitle}
-						</h2>
-					
-					</div>
-				</section>
-				<section className="my-9 text-sm">
-					<h3 className="mb-1 text-slate-900 dark:text-slate-100 font-semibold">
-						About
-					</h3>
-					<div className="text-slate-600 dark:text-slate-300">
-						<p>{generalData.about}</p>
-					</div>
-				</section>
-				<section className="my-14 text-sm">
-					<h3 className="mb-6 text-slate-900 dark:text-slate-100  font-semibold">
-						Contact
-					</h3>
-					<div className="flex flex-col gap-6">
-						{generalData.contacts.map((contact, index) => {
-							return (
-								<div className="flex" key={index}>
-									<div className="mr-8 max-w-[100px] w-full text-slate-400 dark:text-slate-400">
-										{contact.label}
-									</div>
-									<div className="flex flex-col flex-1 text-slate-900 dark:text-slate-100">
-										<a
-											href={contact.href}
-											target="_blank"
-											rel="noopener noreferrer"
-											className="hover:underline inline-flex"
-										>
-											{contact.value}
-											<svg
-												width="12"
-												height="12"
-												viewBox="0 0 12 12"
-												fill="none"
-												xmlns="http://www.w3.org/2000/svg"
-											>
-												<path
-													d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
-													className="fill-current text-slate-900 dark:text-slate-100"
-												></path>
-											</svg>
-										</a>
-									</div>
-								</div>
-							);
-						})}
-					</div>
-				</section>
-				<section className=" text-sm">
-					<h3 className="mb-6 text-slate-900 dark:text-slate-100 font-semibold">
-						Arrange a 30 min meeting through
-					</h3>
-					<div className="text-slate-600 dark:text-slate-300">
-						<a
-							href={"https://calendly.com/shariqwaseem41/30min"}
-							target="_blank"
-							rel="noopener noreferrer"
-							className="hover:underline inline-flex"
-						>
-							Calendly
-							<svg
-								width="12"
-								height="12"
-								viewBox="0 0 12 12"
-								fill="none"
-								xmlns="http://www.w3.org/2000/svg"
-							>
-								<path
-									d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
-									className="fill-current text-slate-900 dark:text-slate-100"
-								></path>
-							</svg>
-						</a>
-					</div>
-				</section>
-				{contentData.map((content, index) => {
-					return <Content {...content} key={index} />;
-				})}
+  return (
+    <>
+      <main className="max-w-3xl mx-auto px-6 py-20 relative min-h-screen font-light">
+        <section className="flex items-center">
+          <Image
+            alt="Author"
+            src="/me.webp"
+            width={110}
+            height={110}
+            className="rounded-full object-cover"
+          />
+          <div className="ml-4">
+            <h1 className="mb-0.5 text-xl text-slate-900 dark:text-slate-100">
+              {generalData.name}
+            </h1>
+            <h2 className="text-slate-600 dark:text-slate-300 text-sm">
+              {generalData.jobTitle}
+            </h2>
+          </div>
+        </section>
+        <section className="my-9 text-sm">
+          <h3 className="mb-1 text-slate-900 dark:text-slate-100 font-semibold">
+            About
+          </h3>
+          <div className="text-slate-600 dark:text-slate-300">
+            <p>{generalData.about}</p>
+          </div>
+        </section>
+        <section className="my-14 text-sm">
+          <h3 className="mb-6 text-slate-900 dark:text-slate-100  font-semibold">
+            Contact
+          </h3>
+          <div className="flex flex-col gap-6">
+            {generalData.contacts.map((contact, index) => {
+              return (
+                <div className="flex" key={index}>
+                  <div className="mr-8 max-w-[100px] w-full text-slate-400 dark:text-slate-400">
+                    {contact.label}
+                  </div>
+                  <div className="flex flex-col flex-1 text-slate-900 dark:text-slate-100">
+                    <a
+                      href={contact.href}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="hover:underline inline-flex"
+                    >
+                      {contact.value}
+                      <svg
+                        width="12"
+                        height="12"
+                        viewBox="0 0 12 12"
+                        fill="none"
+                        xmlns="http://www.w3.org/2000/svg"
+                      >
+                        <path
+                          d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
+                          className="fill-current text-slate-900 dark:text-slate-100"
+                        ></path>
+                      </svg>
+                    </a>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </section>
+        <section className=" text-sm">
+          <h3 className="mb-6 text-slate-900 dark:text-slate-100 font-semibold">
+            Arrange a 30 min meeting through
+          </h3>
+          <div className="text-slate-600 dark:text-slate-300">
+            <a
+              href={"https://cal.com/shariq-waseem-nc8a9e"}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="hover:underline inline-flex"
+            >
+              Cal.com
+              <svg
+                width="12"
+                height="12"
+                viewBox="0 0 12 12"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M3.5 3C3.22386 3 3 3.22386 3 3.5C3 3.77614 3.22386 4 3.5 4V3ZM8.5 3.5H9C9 3.22386 8.77614 3 8.5 3V3.5ZM8 8.5C8 8.77614 8.22386 9 8.5 9C8.77614 9 9 8.77614 9 8.5H8ZM2.64645 8.64645C2.45118 8.84171 2.45118 9.15829 2.64645 9.35355C2.84171 9.54882 3.15829 9.54882 3.35355 9.35355L2.64645 8.64645ZM3.5 4H8.5V3H3.5V4ZM8 3.5V8.5H9V3.5H8ZM8.14645 3.14645L2.64645 8.64645L3.35355 9.35355L8.85355 3.85355L8.14645 3.14645Z"
+                  className="fill-current text-slate-900 dark:text-slate-100"
+                ></path>
+              </svg>
+            </a>
+          </div>
+        </section>
+        {contentData.map((content, index) => {
+          return <Content {...content} key={index} />;
+        })}
 
-				<div className="px-6 absolute left-0 bottom-6">
-					<ThemeSwitch />
-				</div>
-			</main>
-		</>
-	);
+        <div className="px-6 absolute left-0 bottom-6">
+          <ThemeSwitch />
+        </div>
+      </main>
+    </>
+  );
 }
