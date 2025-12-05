a90c8e120c641c7b721b6fe39a2424558bc4e700
Shariq Waseem <sh4riqw@gmail.com>
2025-12-05 20:03:56 +0500

    ...

--- components/Card.tsx
+++ components/Card.tsx
@@ -8,7 +8,7 @@ interface CardProps {
 const Card: React.FC<CardProps> = ({ children, className = "" }) => {
   return (
     <div
-      className={`rounded-xl border border-slate-200 dark:border-slate-800 bg-white dark:bg-slate-950 p-6 transition-all duration-300 hover:shadow-md `}
+      className={`rounded-xl border border-slate-200 dark:border-slate-800 bg-white dark:bg-slate-950 p-6 transition-all duration-300`}
     >
       {children}
     </div>
